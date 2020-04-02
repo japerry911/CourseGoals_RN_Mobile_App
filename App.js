b66a0b16a173146bc39e416ab1e13b6609e4f473
japerry911 <jack.e.perry@outlook.com>
2020-04-02 09:39:32 -0600

    Boilerplate setup

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <View>
+        <TextInput />
+      </View>
+      <View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
