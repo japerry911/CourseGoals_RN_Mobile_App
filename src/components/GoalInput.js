3c76a71910140a9377f7e0932619cd5d2fe50c3f
japerry911 <jack.e.perry@outlook.com>
2020-04-02 16:42:08 -0600

    Finished Styling touches

--- src/components/GoalInput.js
+++ src/components/GoalInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, Button, Modal, Text } from 'react-native';
+import { View, TextInput, StyleSheet, TouchableOpacity, Modal, Text } from 'react-native';
 
 const GoalInput = ({ addGoalHandler, visible, onCancel }) => {
     const [enteredGoal, setEnteredGoal] = useState('');
@@ -12,22 +12,27 @@ const GoalInput = ({ addGoalHandler, visible, onCancel }) => {
     return (
         <Modal visible={visible} animationType="slide">
             <View style={styles.headerViewStyle}>
-                <Text>Enter a Course Goal:</Text>
+                <Text style={styles.headerTextStyle}>Enter a Course Goal:</Text>
                 <TextInput 
                     style={styles.headerInputStyle}
                     placeholder='Course Goal'
                     onChangeText={newGoal => setEnteredGoal(newGoal)}
                     value={enteredGoal}
                 />
-                <Button 
-                    title='ADD' 
-                    onPress={addHandler}
-                />
-                <Button
-                    title='CANCEL'
-                    color='red'
-                    onPress={() => onCancel()}
-                />
+                <View style={styles.buttonViewStyle}>
+                    <TouchableOpacity
+                        style={styles.buttonCancelStyle}
+                        onPress={() => onCancel()}
+                    >
+                        <Text>CANCEL</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.buttonAddStyle} 
+                        onPress={addHandler}
+                    >
+                        <Text>ADD</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </Modal>
     );
@@ -43,8 +48,32 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth: 1,
         width: '80%',
+        height: '5%',
         marginRight: 10,
-        marginBottom: 10
+        marginBottom: 10,
+        padding: 10
+    },
+    buttonViewStyle: {
+        flexDirection: 'row',
+        width: '80%',
+        justifyContent: 'space-between'
+    },
+    buttonAddStyle: {
+        backgroundColor: 'lightgreen',
+        padding: 20,
+        borderRadius: 10,
+        width: '30%',
+        alignItems: 'center'
+    },
+    buttonCancelStyle: {
+        backgroundColor: '#ff6666',
+        padding: 20,
+        borderRadius: 10,
+        width: '30%'
+    },
+    headerTextStyle: {
+        fontSize: 20,
+        paddingBottom: 10
     }
 });
 
