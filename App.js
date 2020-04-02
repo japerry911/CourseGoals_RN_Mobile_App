948607dd10c3926281ac74bf3b30f7c988e1b646
japerry911 <jack.e.perry@outlook.com>
2020-04-02 10:13:19 -0600

    Basic styling/header done/played around with FlexBox

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <View>
-        <TextInput />
+    <View style={styles.mainContainerStyle}>
+      <View style={styles.headerViewStyle}>
+        <TextInput 
+          style={styles.headerInputStyle}
+          placeholder='Course Goal'
+        />
+        <Button title='ADD' />
       </View>
       <View>
       </View>
@@ -13,4 +17,19 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainerStyle: {
+    padding: 50
+  },
+  headerViewStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  headerInputStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    flex: 1,
+    marginRight: 10
+  }
+});
